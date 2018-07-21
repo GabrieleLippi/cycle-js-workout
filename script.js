00ec10460c0cd67e68bbbc849a4c99d9f341acd1
Gabriele Lippi <gabriele.lippi@accurat.it>
2018-07-21 18:49:33 +0200

    Separate logic from effects, and add a logger function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-xs.periodic(1000)
-  .fold(prev => prev + 1, 0)
-  .map(i => `Second elapsed: ${i}`)
-  .subscribe({
+function main() {
+  return xs.periodic(1000)
+    .fold(prev => prev + 1, 0)
+    .map(i => `Second elapsed: ${i}`)
+}
+
+function domDriver(text$) {
+  text$.subscribe({
     next: str => {
       const el = document.querySelector('#app')
       el.textContent = str
     }
   })
+}
+function logDriver(text$) {
+  text$.subscribe({
+    next: str => console.log(str)
+  })
+}
+
+const sink = main()
+domDriver(sink)
+logDriver(sink)
